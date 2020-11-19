d56c17ee78a27b54fc352961b6ff5cb44e7a062b
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2020-11-19 17:42:30 +0800

    Creating Label

--- MarkJustineBalisi.WeatherPanel.Windows/MarkJustineBalisi.WeatherPanel.Windows/obj/Debug/MainWindow.g.i.cs
+++ MarkJustineBalisi.WeatherPanel.Windows/MarkJustineBalisi.WeatherPanel.Windows/obj/Debug/MainWindow.g.i.cs
@@ -40,6 +40,14 @@ namespace MarkJustineBalisi.WeatherPanel.Windows {
     /// </summary>
     public partial class MainWindow : System.Windows.Window, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 10 "..\..\MainWindow.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Label lblSummary;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +76,12 @@ namespace MarkJustineBalisi.WeatherPanel.Windows {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.lblSummary = ((System.Windows.Controls.Label)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
